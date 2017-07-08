275441ac84f5f432bbfb15d61f5fefc629c7bbb2
Ravi <ravimd22@gmail.com>
2017-07-08 21:55:46 +0530

    Local Name Change

--- SampleProject123/SampleProject123/ViewController.swift
+++ SampleProject123/SampleProject123/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func buttonAction(_ sender: Any) {
+    }
+    @IBOutlet weak var button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
